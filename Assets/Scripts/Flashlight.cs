b7b9d06478398dd5f783d529805fd71beca8d90f
MariaMartinezRos <cibernuggets@gmail.com>
2025-03-02 21:40:18 +0100

    flashlight sound

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public Light spotlight;
+
+
+    //SOUND
+    // public AudioSource clickSound; // AudioSource for the click sound
+    public AudioSource loopSound;  // AudioSource for the looping sound
+    // public AudioClip clickClip;    // Sound for both on and off clicks
+    public AudioClip loopClip;     // Sound to loop when flashlight is on
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         spotlight.enabled = false;
+        loopSound.loop = true;
     }
 
     // Update is called once per frame
@@ -19,10 +30,18 @@ public class Flashlight : MonoBehaviour
             if(spotlight.enabled)
             {
                 spotlight.enabled = false;
+
+                // clickSound.PlayOneShot(clickClip); // Play the click sound
+                loopSound.Stop();                  // Stop the loop sound
             }
             else
             {
                 spotlight.enabled = true;
+
+                // clickSound.PlayOneShot(clickClip); // Play the click sound
+                loopSound.clip = loopClip;         // Set the loop sound clip
+                loopSound.Play();                  // Start playing the loop sound
+
             }
         }
     }
